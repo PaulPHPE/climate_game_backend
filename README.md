e7205b3dbcdc96627817be291b6cdeff717a079d
PaulPHPE <93597135+PaulPHPE@users.noreply.github.com>
2024-04-29 18:07:43 +0200

    feat: set up postgres

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # CLIMATE GAME BACKEND
 
+## Set up database
+
+- Create a local postgresql Database : `CREATE DATABASE climgame_db;`
+- Create User : `CREATE USER clim WITH PASSWORD 'password';`
+- Grant priviledges : `GRANT ALL PRIVILEGES ON DATABASE climgame_db TO clim;`
+- Then initialize the DB by running the following command : `python init_db.py`
+
 ## Develop Localy
 
 - First : set up local environment `python -m venv venv`
 - Second : activate virtual environment `source ./venv/bin/activate`
-- Third : install dependencies `pip install -r dependencies.txt`
+- Third : install dependencies `pip install -r requirements.txt`
---
+++ init_db.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
+url = url = os.getenv("DATABASE_URL")
+conn = psycopg2.connect(url)
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# Execute a command: this creates a new table
+cur.execute('CREATE TABLE IF NOT EXISTS games (id serial PRIMARY KEY,'
+                                 'created_at date DEFAULT CURRENT_TIMESTAMP);'
+                                 )
+cur.execute('CREATE TABLE IF NOT EXISTS game_years (id serial PRIMARY KEY,'
+                                 'year integer NOT NULL,'
+                                 'state text NOT NULL,'
+                                 'updated_at date,'
+                                 'created_at date DEFAULT CURRENT_TIMESTAMP);'
+                                 )
+
+conn.commit()
+
+cur.close()
+conn.close()
